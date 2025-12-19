dc425afe5f405130f2f7d30a9df5b88f510e9286
usmanumer038 <usmanumer286@gmail.com>
2025-12-19 20:36:12 +0500

    Fix vercel-build.sh to pure bash

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,10 +1,8 @@
-Set-Content -Path vercel-build.sh -NoNewline @"
 #!/usr/bin/env bash
 set -e
 git clone https://github.com/flutter/flutter.git --depth 1
-export PATH="\$PATH:$(pwd)/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
 
 flutter config --enable-web
 flutter pub get
 flutter build web --release --no-wasm-dry-run
-"@
