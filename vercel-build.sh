6bc6a5d605201d9b8254c0839ad0f967ac331cd2
usmanumer038 <usmanumer286@gmail.com>
2025-12-19 20:41:38 +0500

    Pin Flutter 3.38.5 for Vercel build

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,8 +1,10 @@
+Set-Content -Path vercel-build.sh -NoNewline @"
 #!/usr/bin/env bash
 set -e
-git clone https://github.com/flutter/flutter.git --depth 1
-export PATH="$PATH:$(pwd)/flutter/bin"
+git clone https://github.com/flutter/flutter.git --depth 1 -b 3.38.5
+export PATH="\$PATH:$(pwd)/flutter/bin"
 
 flutter config --enable-web
 flutter pub get
 flutter build web --release --no-wasm-dry-run
+"@
