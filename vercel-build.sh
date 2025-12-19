c0a6c5ad61cfbdf6d842e2861bb59e3e318223d5
usmanumer038 <usmanumer286@gmail.com>
2025-12-19 20:48:24 +0500

    Fix vercel-build.sh to pure bash and pin Flutter 3.38.5

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,10 +1,10 @@
-Set-Content -Path vercel-build.sh -NoNewline @"
+@'
 #!/usr/bin/env bash
 set -e
 git clone https://github.com/flutter/flutter.git --depth 1 -b 3.38.5
-export PATH="\$PATH:$(pwd)/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
 
 flutter config --enable-web
 flutter pub get
 flutter build web --release --no-wasm-dry-run
-"@
+'@ | Set-Content -Path vercel-build.sh -NoNewline -Encoding UTF8
